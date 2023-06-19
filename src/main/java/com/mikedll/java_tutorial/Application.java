b60bc019d8dd36d7c8e93650b669a6e35a3f3831
Michael Rivera <soymrmike@gmail.com>
2023-06-18 19:11:32 -0700

    Initial commit of java playground.

---
+++ src/main/java/com/mikedll/java_tutorial/Application.java
@@ -0,0 +1,12 @@
+package com.mikedll.java_tutorial;
+
+import java.util.List;
+import java.util.Optional;
+import org.javatuples.Pair;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello everyone");
+    }
+}
